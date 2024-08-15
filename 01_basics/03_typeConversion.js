ddecb1be757dfc27e9a14e16a94782cfd600eeff
Ravi kant Dubey <121443863+Mrdubey0@users.noreply.github.com>
2024-08-15 17:01:51 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,37 @@
+let score = "rabi"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+//"33" =>33
+//"33abc" => NaN type is Number
+//true =>1;false=>0
+
+let isLoggedin = "ravi"
+let booleanIsLoggedIn = Boolean(isLoggedin)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0=>False
+//"" = > False
+//"ravi" = > True
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+console.log(SomeNumber);
+console.log(typeof StringNumber);
+
+
+
+
+
+
+
+
+
+
