6768c645a40b9e33bb808ac23c1d74fe7890b603
Ravi kant Dubey <121443863+Mrdubey0@users.noreply.github.com>
2024-08-16 05:21:51 +0000

    conversion

--- 01_basics/03_typeConversion.js
+++ 01_basics/03_typeConversion.js
@@ -27,6 +27,48 @@ console.log(SomeNumber);
 console.log(typeof StringNumber);
 
 
+// ***************Operations******************** /
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "ravi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+console.log(+"");
+
+let GameCounter = 100
+GameCounter++;
+console.log(GameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
